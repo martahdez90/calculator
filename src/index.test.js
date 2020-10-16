06818fe4a402376ce6e6930aec5a8a16abeff7da
Marta H <65173504+martahdez90@users.noreply.github.com>
2020-10-16 17:45:36 +0200

    testing pruebas

--- src/index.test.js
+++ src/index.test.js
@@ -8,18 +8,52 @@
 //   expect(result).toBe(theNum);
 // })
 
-const { setNum, moveNum, displayNum, clearAll }= require("./app");
+const { setNum, moveNum, displayNum, clearAll } = require("./app");
 
-// let setNum = () => {
-//     // If a result was displayed, reset number
-//     if (resultNum) {
-      
-//       theNum = this.getAttribute("data-num");
-//       resultNum = "";
-//     } else {
-//       // Otherwise, add digit to previous number (this is a string!)
-//       theNum += this.getAttribute("data-num");
-//     }
+test("if number are right", () => {
+  let oldNum = 5
+  let theNum = 3
+  let operator = "plus"
+  expect(displayNum()).toBe(8)
+})
+let displayNum = () => {
+  // Convert string input to numbers
+  oldNum = parseFloat(oldNum);
+  theNum = parseFloat(theNum);
 
-//     viewer.innerHTML = theNum; // Display current number
-//   };
+  // Perform operation
+  switch (operator) {
+    case "plus"://sum
+      resultNum = oldNum + theNum;
+      break;
+    
+    case "minus"://subtraction
+      resultNum = oldNum - theNum;
+      break;
+    
+    // If equal is pressed without an operator, keep number and continue
+    default:
+      resultNum = theNum;
+  }
+
+  // If NaN or Infinity returned
+  if (!isFinite(resultNum)) {
+    if (isNaN(resultNum)) {
+      // If result is not a number; set off by, eg, double-clicking operators
+      resultNum = "You broke it!";
+    } else {
+      // If result is infinity, set off by dividing by zero
+      resultNum = "Look at what you've done";
+      el("#calculator").classList.add("broken"); // Break calculator
+    }
+  }
+
+  // Display result, finally!Yes!!!
+  viewer.innerHTML = resultNum;
+  equals.setAttribute("data-result", resultNum);
+
+  // Now reset oldNum & keep result
+  oldNum = 0;
+  theNum = resultNum;
+  return resultNum;
+};
