51803af31dd096268ef893e96a22bc1eccaeeca8
Marta H <65173504+martahdez90@users.noreply.github.com>
2020-10-17 18:13:52 +0200

    first test puppeteer

--- src/app.test.js
+++ src/app.test.js
@@ -1,6 +1,9 @@
 //the test runner used is jest
 //yarn add --dev jest
 //yarn test
+//yarn add --dev puppeteer
+
+
 
 // const setNum = require("./app");
 // const result = setNum()
@@ -8,5 +11,20 @@
 //   expect(result).toBe(theNum);
 // })
 
-const { setNum, moveNum, displayNum, clearAll } = require("./app");
+const puppeteer = require('puppeteer');
+// const { clickNumbers, clickOperators, clickEqual, clickClear, setNum, moveNum, displayNum, clearAll } = require("./app");
 
+test('sum of a + b should be c', async () => {
+    //start the browser
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 80,
+        args: ['--window-size=1920, 1080']
+    })
+    //create and open the page
+    const page = await browser.newPage()
+    await page.goto(
+        'http://127.0.0.1:5500/src/index.html'
+    )
+
+})
