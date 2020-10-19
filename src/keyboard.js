4b86fa49a8d3f2d3dd98349c57cef334d59b7fb2
Marta H <65173504+martahdez90@users.noreply.github.com>
2020-10-19 20:26:14 +0200

    keyboard works

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,8 +1,75 @@
-let keyboard = () => {
- window.addEventListener('keydown', (event) => {
-     document.getElementById('viewer').innerHTML = event.key 
-       return (event.key)
-});   
-}
+let theNum = "",
+  oldNum = "",
+  keyPressed = "key", //key pressed
+  resultNum,
+  operator;
 
-//keyboard()
+//When: key is press. Get the current number selected
+let keyboard = () => {
+  window.addEventListener(
+    "keydown",
+    (event) => {
+      keyPressed = event.key;
+      console.log(keyPressed)
+      //if the keypressed is a number
+      if (
+        keyPressed === "0" ||
+        keyPressed === "1" ||
+        keyPressed === "2" ||
+        keyPressed === "3" ||
+        keyPressed === "4" ||
+        keyPressed === "5" ||
+        keyPressed === "6" ||
+        keyPressed === "7" ||
+        keyPressed === "8" ||
+        keyPressed === "9"
+      ) {
+        if (resultNum) {
+          // If a result was displayed, reset number
+          theNum = keyPressed;
+          resultNum = "";
+        } else {
+          // Otherwise, add digit to previous number (this is a string!)
+          theNum += keyPressed;
+        }
+      } else {
+        //if keyPress it's not a number
+        switch (keyPressed) {
+          case "+":
+            oldNum = theNum;
+            theNum = "";
+            operator = "plus";
+            break;
+          case "-":
+            oldNum = theNum;
+            theNum = "";
+            operator = "minus";
+            break;
+          case "*":
+            oldNum = theNum;
+            theNum = "";
+            operator = "mult";
+            break;
+          case "/":
+            oldNum = theNum;
+            theNum = "";
+            operator = "div";
+            break;
+            case "c":
+              clearAll();
+              break;
+            case "Enter"://press start 
+              displayNum();
+              break;
+          // If KeyPressed is not a number;
+          default:
+            document.getElementById("viewer").innerHTML = "solo numeros";
+        }
+      }
+    }
+  );
+};
+
+keyboard();
+
+document.getElementById("viewer").innerHTML = resultNum;
--- src/keyboard.test.js
+++ src/keyboard.test.js
@@ -2,7 +2,36 @@ const puppeteer = require('puppeteer');
 
 
 describe("Test de keyboard", () => {
-    test("keys", async () => {
+    test("number", async () => {
+        //start the browser
+        const browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 50,
+            args: ['--window-size=1920, 1200']
+        });
+        //create and open the page
+        const page = await browser.newPage();
+        //go to my app
+        await page.goto(
+            'http://localhost:8082/src/index.html'
+        )
+        //type the number
+        await page.type('#viewer', '1');
+        await page.type('#viewer', '2');
+        await page.type('#viewer', '3');
+        await page.type('#viewer', '4');
+        await page.type('#viewer', '5');
+        await page.type('#viewer', '6');
+        await page.type('#viewer', '7');
+        await page.type('#viewer', '8');
+        await page.type('#viewer', '9');
+        await page.type('#viewer', '0');
+        // await expect(page).toClick('button', { text: '2' })
+        //resolve
+        const testResult = await page.$eval('#viewer', el => el.textContent)
+        expect(testResult).toBe('0')
+    }, 30000);
+    test("sum", async () => {
         //start the browser
         const browser = await puppeteer.launch({
             headless: false,
